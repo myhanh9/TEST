9be9ef7fbf09ce95fc2d69011cb3827dfbcaa79a
Trinh Thi My Hanh <trinhthimyhanh.mh9@gmail.com>
2023-02-12 17:15:43 +0700

    Update Ex2

--- Ex2.py
+++ Ex2.py
@@ -1,1 +1,12 @@
-#Lam gi cung dc
+flag = True
+while(flag):
+    n = input("Enter number of Fibonacci: , Enter '0' to exit")
+    if n.isnumeric() is False:
+        print("Please enter a number only!")
+    elif int(n) == 0:
+        flag = False
+    else:
+        Fib_list = [1, 1]
+        for i in range(int(n)-2):
+            Fib_list.append(Fib_list[len(Fib_list)-1] + Fib_list[len(Fib_list)-2])
+        print(Fib_list)
